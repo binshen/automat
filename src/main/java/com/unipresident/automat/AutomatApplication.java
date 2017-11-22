bae8f3719fe0febdf14822e01ec286e73ca02800
沈斌 <bluelazysb@hotmail.com>
2017-11-22 22:48:26 +0800

    updated

--- src/main/java/com/unipresident/automat/AutomatApplication.java
+++ src/main/java/com/unipresident/automat/AutomatApplication.java
@@ -2,11 +2,18 @@ package com.unipresident.automat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutomatApplication {
+public class AutomatApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AutomatApplication.class);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(AutomatApplication.class, args);
 	}
 }
